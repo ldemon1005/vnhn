d6032894b74defda143c66d3ab72a740f037c6c2
ldemon <ldemon1005@gmail.com>
2018-07-12 16:53:17 +0700

    tuan 7/12

---
+++ local/resources/views/client/layouts/header.blade.php
@@ -0,0 +1,66 @@
+<div id="header">
+    <section id="header-top">
+        <div class="container">
+            <div class="row ">
+                <div class="top-header">
+                    <div class="e-magazine">
+                        <a><img src="{{asset('/local/resources/uploads/images/e-magazine.png')}}"></a>
+                    </div>
+                    <div class="top-header-menu">
+                        <ul class="menu-left">
+                            <li class="header-lang"><a href="#"><p><i class="fas fa-phone"></i>096 432 8383</p></a></li>
+                            <li class="separation"><img src="{{asset('/local/resources/uploads/images/cham.png')}}"></li>
+                            <li class="header-lang"><a href="#"><p><img src="{{asset('/local/resources/uploads/images/ads-icon.png')}}">Quảng cáo</p></a></li>
+                            <li class="separation"><img src="{{asset('/local/resources/uploads/images/cham.png')}}"></li>
+                            <li class="list-icon">
+                                <a href=""><i class="fab fa-facebook-f"></i></a>
+                                <a href=""><i class="fab fa-youtube"></i></a>
+                                <a href=""><i class="fas fa-envelope"></i></a>
+                                <a href=""><i class="fas fa-search" style="color: #303840"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="header-banner">
+        <div class="container">
+            <div class="row">
+                <div class="logo-vnhn">
+                    <a href="#"><img src="{{asset('local/resources/uploads/images/logo-vnhn.png')}}"></a>
+                </div>
+                <div class="quangcao-1">
+                    <a href="#"><img src="{{asset('local/resources/uploads/images/quang-cao-1.png')}}"></a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="header-menu">
+        <div class="container">
+            <div class="row">
+                <ul class="menu-header">
+                    <li style="border-left: 0px"><a href=""><i class="fas fa-home"></i></a></li>
+                    <li><a href="">Media</a></li>
+                    <li><a href="">Thời sự</a></li>
+                    <li><a href="">Thế giới</a></li>
+                    <li><a href="">Kinh doanh</a></li>
+                    <li><a href="">Từ chính sách tới thực tiễn</a></li>
+                    <li><a href="">Khoa học - Công nghệ</a></li>
+                    <li><a href="">Số hóa</a></li>
+                    <li><a href="">Nghiên cứu trao đổi</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                           aria-expanded="false">Danh mục khác <span class="caret"></span></a>
+                        <ul class="dropdown-menu dropdown-menu-right" style="background-color: #d71a21">
+                            <li><a href="#">Danh mục khác</a></li>
+                            <li><a href="#">Thời sự</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+</div>
