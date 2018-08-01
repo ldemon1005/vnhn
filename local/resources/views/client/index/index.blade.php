07af437c340c16087b2bc06d05c10cd4d0ac06b6
vquyen96 <quyendvd00775@fpt.edu.vn>
2018-08-01 10:35:42 +0700

    oke

--- local/resources/views/client/index/index.blade.php
+++ local/resources/views/client/index/index.blade.php
@@ -363,7 +363,7 @@
                         <div class="quangcao-group">
                             <section class="new-right-1">
                                 <div class="category">
-                                    <h3>{{\Illuminate\Support\Facades\Config::get('') == 'vn' ? 'Tạp chí thường kỳ' : 'Regular magazine'}}</h3>
+                                    <h3>{{\Illuminate\Support\Facades\Config::get('app.locale') == 'vn' ? 'Tạp chí thường kỳ' : 'Regular magazine'}}</h3>
                                 </div>
                                 <div class="slide">
 
